edca3c1ccc63f7af6641333d58afda5680807af5
umair.zafar@tenpearls.com <umair.zafar@tenpearls.com>
2019-07-03 12:24:21 +0500

    removed mongo calls

--- WebRTC/Controllers/HomeController.cs
+++ WebRTC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
 using Conference.Data.Repository;
+using System.Collections.Generic;
 
 namespace WebRTC.Controllers
 {
@@ -12,79 +13,27 @@ namespace WebRTC.Controllers
         RepositoryBase<Conference.Entities.Conference> conferenceRepo = new RepositoryBase<Conference.Entities.Conference>();
 
         public ActionResult Index()
-        {
-            //Conference.Entities.Conference obj = new Conference.Entities.Conference();
-            //obj.CreatedBy = "abc";
-            //obj.CreatedOn = DateTime.Now.Date;
-            //obj.IsPrivate = false;
-            //obj.Title = "unique 1";
-            //obj.SessionId = "unique 1";
-            //conferenceRepo.Insert(obj);
-
-            //obj = new Conference.Entities.Conference();
-            //obj.CreatedBy = "abc";
-            //obj.CreatedOn = DateTime.Now.Date;
-            //obj.IsPrivate = false;
-            //obj.Title = "unique 2";
-            //obj.SessionId = "unique 2";
-            //conferenceRepo.Insert(obj);
-
-            //obj = new Conference.Entities.Conference();
-            //obj.CreatedBy = "abc";
-            //obj.CreatedOn = DateTime.Now.Date;
-            //obj.IsPrivate = false;
-            //obj.Title = "unique 3";
-            //obj.SessionId = "unique 3";
-            //conferenceRepo.Insert(obj);
-
-
-            //obj = new Conference.Entities.Conference();
-            //obj.CreatedBy = "abc";
-            //obj.CreatedOn = DateTime.Now.Date;
-            //obj.IsPrivate = false;
-            //obj.Title = "unique 4";
-            //obj.SessionId = "unique 4";
-            //conferenceRepo.Insert(obj);
-
-            //obj = new Conference.Entities.Conference();
-            //obj.CreatedBy = "abc";
-            //obj.CreatedOn = DateTime.Now.Date;
-            //obj.IsPrivate = false;
-            //obj.Title = "unique 5";
-            //obj.SessionId = "unique 5";
-            //conferenceRepo.Insert(obj);
-
-            //obj = new Conference.Entities.Conference();
-            //obj.CreatedBy = "abc";
-            //obj.CreatedOn = DateTime.Now.Date;
-            //obj.IsPrivate = false;
-            //obj.Title = "unique 6";
-            //obj.SessionId = "unique 6";
-
-            //conferenceRepo.Insert(obj);
-
-            var model = conferenceRepo.GetAll();
+        {           
             return View();            
         }
 
         [HttpGet]
         public ActionResult GetAllConferences()
         {
-            var model = conferenceRepo.GetAll();
-            return PartialView("AllConference",model);
+           // var model = conferenceRepo.GetAll();
+            return PartialView("AllConference",new List<Conference.Entities.Conference>());
         }
 
 
         [HttpPost]
         public JsonResult CreateConference(Conference.Entities.Conference model)
         {
-            var insert = conferenceRepo.Insert(model);
-            if (insert != null)
-            {
-               // var conferenceList = conferenceRepo.GetAll();
-                return new JsonResult(){  Data = insert, JsonRequestBehavior= JsonRequestBehavior.AllowGet };
-            }
-            else
+            //var insert = conferenceRepo.Insert(model);
+            //if (insert != null)
+            //{               
+            //    return new JsonResult(){  Data = insert, JsonRequestBehavior= JsonRequestBehavior.AllowGet };
+            //}
+            //else
                 return new JsonResult() { Data = false, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
 
         }
